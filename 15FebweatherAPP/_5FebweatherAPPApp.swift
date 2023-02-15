6ae3ae725fbfd652d76891c59725e353c66760f4
KhadijahMawlan <2000khadijah1420@gmail.com>
2023-02-15 13:03:49 +0300

    Initial Commit

---
+++ 15FebweatherAPP/_5FebweatherAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5FebweatherAPPApp.swift
+//  15FebweatherAPP
+//
+//  Created by خديجه عبدالناصر on 24/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct _5FebweatherAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
